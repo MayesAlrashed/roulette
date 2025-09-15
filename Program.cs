f8f9c221fe5d6c0b98fa68f177f48aa94a348802
15MA01010 <15MA01010@KB-5CG4191Q6B>
2025-09-15 11:01:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter the amount you want to bet");
+string input=Console.ReadLine();
+int amount= Convert.ToInt32(input);
+
+Random rand = new Random();
+for (int i = 0; i < amount; i++)
