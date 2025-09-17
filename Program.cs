8e4b437e6989c644284aa5d74ce3971663edaf43
Mayes Alrashed <15MA01010@amal.se>
2025-09-17 09:11:15 +0200

    base done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 ﻿Console.WriteLine("Enter the amount you want to bet");
-string input=Console.ReadLine();
-int amount= Convert.ToInt32(input);
+string money=Console.ReadLine();
+int bet=Convert.ToInt32(money);
 
+Console.WriteLine("Enter the number you want to bet on");
+string input=Console.ReadLine();
+int guess= Convert.ToInt32(input);
+
+
+//List<int> list=new List<int>();
 Random rand = new Random();
-for (int i = 0; i < amount; i++)
+int randNumb =rand.Next(1,4);
+
+if (randNumb ==guess)
+{
+    Console.WriteLine("You won " + 2*bet);
+} else
+{
+    Console.WriteLine("You lost");
+}
+
